192fb25fa8fb75b62d4d5cdd350d6e12ab83bf9b
Uzurch1k <galcenkovlad7@gmail.com>
2025-03-08 21:30:20 +0200

    fix

--- src/js/parts/scroll.js
+++ src/js/parts/scroll.js
@@ -1,23 +1,10 @@
-import scrollToElement from 'scroll-to-element';
+const anchorBtns = document.querySelectorAll('.anchor');
 
-const anchorLinks = document.querySelectorAll('.footer__btns a');
-
-anchorLinks?.forEach(link => {
-  link.addEventListener('click', event => {
-    const headerHeight = document.querySelector('.header').offsetHeight;
-    const href = link.getAttribute('href');
-
-    if (href.startsWith('#')) {
-      event.preventDefault();
-
-      const targetElement = document.querySelector(href);
-      if (targetElement) {
-        scrollToElement(targetElement, {
-          offset: -headerHeight, // Смещение от элемента (если нужно добавить отступ)
-          ease: 'inOutQuint', // Плавность анимации
-          duration: 1000, // Длительность анимации (мс)
-        });
-      }
-    }
+anchorBtns?.forEach(btn => {
+  btn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
   });
 });
