7679b14cff3604e71aea8ad62e65e0ba3d460b20
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2025-03-17 15:52:07 +0200

    Title

---
+++ src/js/parts/title.js
@@ -0,0 +1,49 @@
+export const talent = () => {
+    const section = document.querySelector('.section__talent');
+  
+    let delay = 0;
+  
+    if (section) {
+      const title = document.querySelector('.talent__title');
+      const letters = title.textContent.split(''); // Розбиваємо текст на букви
+      title.innerHTML = letters.map(letter => `<span>${letter}</span>`).join(''); // Обгортаємо кожну букву в <span>
+  
+      const spans = document.querySelectorAll('.talent__title span');
+  
+      window.addEventListener('scroll', () => {
+        const sectionRect = section.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+  
+        if (section?.classList.contains('noAnim')) {
+          setTimeout(() => {
+            delay = 300;
+            section.classList.remove('noAnim');
+          }, 100);
+        }
+  
+        // Обчислюємо прогрес скролу: 0 коли верх секції торкається верхнього краю екрану,
+        // і 1 коли низ секції торкається нижнього краю екрану
+        const progress = Math.min(
+          Math.max(
+            (windowHeight - sectionRect.bottom + 100) /
+              (windowHeight - sectionRect.height),
+            0
+          ),
+          1
+        );
+  
+        // Кількість букв для замальовування
+        const lettersToPaint = Math.floor(progress * spans.length);
+  
+        // Замальовуємо відповідну кількість букв
+        spans.forEach((span, index) => {
+          if (index >= spans.length - lettersToPaint) {
+            span.style.color = '#CDCDCD'; // Замальовуємо в чорний
+          } else {
+            span.style.color = '#191919'; // Повертаємо в сірий
+          }
+        });
+      });
+    }
+  };
+  
